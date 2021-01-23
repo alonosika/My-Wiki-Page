3634957f8ddd53e9cd005ef5689529bb6cb5703a
alonosika <alonosika@gmail.com>
2021-01-23 17:13:44 +0200

    add

---
+++ js homework/q1.js
@@ -0,0 +1,9 @@
+//Q1 Answer
+let num1 = Number(prompt("insert Number1"));
+let num2 = Number(prompt("insert Number2"));
+if(num1 > num2) {
+    alert(num1);
+} 
+    else{
+        alert(num2);
+    }
